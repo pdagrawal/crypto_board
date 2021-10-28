919a422b89a3bba7c3c3c3c42261c4fc7a71f064
Pradeep Agrawal <p98@umbc.edu>
2021-10-28 19:09:16 -0400

    Added about, contact in the navbar and made them dynamic

--- crypto_board/views.py
+++ crypto_board/views.py
@@ -1,6 +1,10 @@
-from django.http import HttpResponse
-from django.template import loader
 from django.shortcuts import render
 
 def index(request):
     return render(request, 'index.html')
+
+def about(request):
+    return render(request, 'about.html')
+
+def contact(request):
+    return render(request, 'contact.html')
