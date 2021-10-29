1d3e2c78dd327bc81abe3ea74933f40d500e5b5e
Pradeep Agrawal <p98@umbc.edu>
2021-10-28 21:42:45 -0400

    Login and logout functionality

--- crypto_board/urls.py
+++ crypto_board/urls.py
@@ -26,6 +26,7 @@ urlpatterns = [
     path('contact', views.contact, name="contact"),
 
     # Django Auth
-    path('accounts/login', auth_views.LoginView.as_view(template_name="accounts/login.html"), name="login")
+    path('accounts/login', auth_views.LoginView.as_view(template_name="accounts/login.html"), name="login"),
+    path('accounts/logout', auth_views.LogoutView.as_view(), name='logout')
 ]
 # path('accounts/', include('django.contrib.auth.urls'))
