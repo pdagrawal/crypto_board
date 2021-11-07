6e3b5e7af033626b9b1f12878472bed7835df71f
Pradeep Agrawal <p98@umbc.edu>
2021-11-06 22:26:25 -0400

    Added profile view

--- crypto_board/urls.py
+++ crypto_board/urls.py
@@ -24,6 +24,7 @@ urlpatterns = [
     path('', views.index, name="index"),
     path('about', views.about, name="about"),
     path('contact', views.contact, name="contact"),
+    path('accounts/profile', views.ProfileView.as_view(), name="profile"),
 
     # Django Auth
     path('accounts/login', auth_views.LoginView.as_view(template_name="accounts/login.html"), name="login"),
