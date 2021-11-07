54c806eb742f2690c06aa5865fca87850971e84d
Pradeep Agrawal <p98@umbc.edu>
2021-11-07 01:29:19 -0400

    Added accounts and public urls and separated the code

--- crypto_board/views.py
+++ crypto_board/views.py
@@ -1,19 +1,5 @@
-from django.shortcuts import render
-from django.http import HttpRequest, HttpResponse
 from django.views.generic.base import TemplateView
 from django.contrib.auth.mixins import LoginRequiredMixin
-
-
-def index(request: HttpRequest) -> HttpResponse:
-    return render(request, "index.html")
-
-
-def about(request: HttpRequest) -> HttpResponse:
-    return render(request, "about.html")
-
-
-def contact(request: HttpRequest) -> HttpResponse:
-    return render(request, "contact.html")
 
 
 class ProfileView(LoginRequiredMixin, TemplateView):
