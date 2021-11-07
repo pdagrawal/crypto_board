a9e28f88d5867fd278821470ea53133641aa9ec8
Pradeep Agrawal <p98@umbc.edu>
2021-11-07 00:55:02 -0400

    Using HttpRequest and HttpResponse libraries

--- crypto_board/views.py
+++ crypto_board/views.py
@@ -1,17 +1,18 @@
 from django.shortcuts import render
+from django.http import HttpRequest, HttpResponse
 from django.views.generic.base import TemplateView
 from django.contrib.auth.mixins import LoginRequiredMixin
 
 
-def index(request):
+def index(request: HttpRequest) -> HttpResponse:
     return render(request, "index.html")
 
 
-def about(request):
+def about(request: HttpRequest) -> HttpResponse:
     return render(request, "about.html")
 
 
-def contact(request):
+def contact(request: HttpRequest) -> HttpResponse:
     return render(request, "contact.html")
 
 
