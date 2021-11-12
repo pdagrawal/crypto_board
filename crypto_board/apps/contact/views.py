9f65064b4d19bdd487e88a85fe80493ecb91753d
Pradeep Agrawal <p98@umbc.edu>
2021-11-12 14:42:23 -0500

    implemented the contact form functionality

--- crypto_board/apps/contact/views.py
+++ crypto_board/apps/contact/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpRequest, HttpResponse
+from django.core.mail import send_mail
+
+from crypto_board import settings
 
 from .forms import ContactForm
 
@@ -9,7 +12,11 @@ def contact(request: HttpRequest) -> HttpResponse:
     elif request.method == 'POST':
         form = ContactForm(request.POST)
         if form.is_valid():
-            pass
+            name = form.cleaned_data["name"]
+            email = form.cleaned_data["email"]
+            message = form.cleaned_data["message"]
+            send_mail(f"{name} sent an email", message, email, [settings.DEFAULT_FROM_EMAIL])
+            return render(request, "contact.html", {"form": ContactForm(), "success": True})
     else:
         raise NotImplementedError
     return render(request, "contact.html", {"form": form})
