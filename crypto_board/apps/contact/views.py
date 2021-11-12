e60e6967800510e4678bf720c386d1be179044f3
Pradeep Agrawal <p98@umbc.edu>
2021-11-12 14:26:56 -0500

    rendering the contact form dynamically using django

--- crypto_board/apps/contact/views.py
+++ crypto_board/apps/contact/views.py
@@ -1,5 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpRequest, HttpResponse
 
+from .forms import ContactForm
+
 def contact(request: HttpRequest) -> HttpResponse:
-    return render(request, "contact.html")
+    if request.method == 'GET':
+        form = ContactForm()
+    elif request.method == 'POST':
+        form = ContactForm(request.POST)
+        if form.is_valid():
+            pass
+    else:
+        raise NotImplementedError
+    return render(request, "contact.html", {"form": form})
