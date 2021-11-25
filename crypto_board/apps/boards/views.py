0e597f723c1596661a342b0118ce062a40f3d404
Pradeep Agrawal <p98@umbc.edu>
2021-11-24 20:39:45 -0500

    Added login check for boards

--- crypto_board/apps/boards/views.py
+++ crypto_board/apps/boards/views.py
@@ -1,17 +1,22 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 
 from crypto_board.apps.boards.models import Board
 
+@login_required
 def index(request):
     boards = Board.objects.all().order_by('-created_at')
     return render(request, "boards/index.html", {'boards': boards})
 
+@login_required
 def show(request, id):
     board = Board.objects.get(pk=id)
     return render(request, "boards/show.html", {'board': board})
 
+@login_required
 def edit(request, id):
     return render(request, "boards/edit.html")
 
+@login_required
 def board_versions(request, id):
     return render(request, "boards/board_versions.html")
